319696b8775a9347bdd43967689d1748d33457e8
Luis Felipe Martins Pereira <luis.f.pereira@ufv.br>
2025-07-24 12:37:13 -0300

    Criação do schema.sql e toda estrutura de BD, C# e WPF para comecar implementacao

---
+++ Models/Relatorio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChronoLabel.Models;
+
+public partial class Relatorio
+{
+    public int IdRelatorio { get; set; }
+
+    public string CpfUsuario { get; set; } = null!;
+
+    public string IdProduto { get; set; } = null!;
+
+    public int QuantidadeProdutoOperado { get; set; }
+
+    public DateTime DataCriacao { get; set; }
+
+    public DateTime DataTermino { get; set; }
+
+    public TimeSpan Duracao { get; set; }
+
+    public virtual Usuario CpfUsuarioNavigation { get; set; } = null!;
+
+    public virtual Produto IdProdutoNavigation { get; set; } = null!;
+}
